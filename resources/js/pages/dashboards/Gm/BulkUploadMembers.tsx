dca64502c8321d1a6ec9876562023f7bdae36bc3
Drwillieong <kevincorpuz321@gmail.com>
2026-08-25 01:27:25 +0800

    Update composer dependencies and refine UI breadcrumbs in BulkUploadMembers and Password components

--- resources/js/pages/dashboards/Gm/BulkUploadMembers.tsx
+++ resources/js/pages/dashboards/Gm/BulkUploadMembers.tsx
@@ -40,7 +40,7 @@ import { type BreadcrumbItem } from '@/types';
 
 const breadcrumbs: BreadcrumbItem[] = [
     { title: 'Dashboard', href: '/dashboard' },
-    { title: 'Member Validation', href: '/dashboards/Gm/MemberValidate' },
+    
     { title: 'Bulk Upload', href: '/dashboards/Gm/BulkUploadMembers' },
 ];
 
